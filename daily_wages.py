74a423b1384f477718ceee12e434a6cdfd3c89ca
NarrowPathDev <jeansejour1994@gmail.com>
2026-09-09 13:42:33 -0400

    Implement wage calculation with Sunday multiplier

---
+++ daily_wages.py
@@ -0,0 +1,9 @@
+hourly_wage = float(input("Hourly wage: "))
+hours_worked = int(input("Hours worked: "))
+day_of_week = input("Day of the week: ")
+
+daily_wages = hourly_wage * hours_worked
+
+if day_of_week == "Sunday":
+    daily_wages = daily_wages * 2
+print(f"Daily wages: {daily_wages} euros")
