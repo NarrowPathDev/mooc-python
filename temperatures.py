ab048ea810715e90620176412002266427f02ea3
NarrowPathDev <jeansejour1994@gmail.com>
2026-09-09 13:29:30 -0400

    Solve temperatures logic with freezing threshold

---
+++ temperatures.py
@@ -0,0 +1,8 @@
+temp = int(input("Please type in a temperature (F): "))
+
+celsius = (temp - 32) / 1.8
+
+print(f"{temp} degrees Fahrenheit equals {celsius} degrees Celsius")
+
+if celsius < 0:
+    print("Brr! It's cold in here!")
