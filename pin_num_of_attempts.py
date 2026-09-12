770915ce9ce807266ca789c837a2b96663d8636c
NarrowPathDev <29716092+NarrowPathDev@users.noreply.github.com>
2026-09-12 09:16:47 -0400

    Complete PIN and number of attempts exercise

--- pin_num_of_attempts.py
+++ pin_num_of_attempts.py
@@ -2,12 +2,13 @@ attempts = 0
 
 while True:
     pin = int(input("PIN: "))
-
+    attempts += 1
 
     if pin == 4321:
-        attempts += 1
-        print(f"Correct! It only took you one single attempt!")
+        if attempts == 1:
+            print(f"Correct! It only took you one single attempt!")
+        else:
+            print(f"Correct! It took you {attempts} attempts")
         break
     elif pin != 4321:
-        attempts += 1
         print("Wrong")
