0857de0465e5bdb0c95d7b602c3a7bf4be5ec52f
NarrowPathDev <29716092+NarrowPathDev@users.noreply.github.com>
2026-09-11 23:05:14 -0400

    feat: implement password matching loop

---
+++ repeat_password.py
@@ -0,0 +1,9 @@
+password = input("Password: ")
+
+while True:
+    repeat_password = input("Repeat password: ")
+    if password != repeat_password:
+        print("They do not match!")
+    else:
+        print("User account created!")
+        break
