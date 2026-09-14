85faa22e69235a0af1ee1f69ad0d9c68fbd18b96
NarrowPathDev <29716092+NarrowPathDev@users.noreply.github.com>
2026-09-13 21:54:22 -0400

    feat: add longer string comparison

---
+++ the_longer_string.py
@@ -0,0 +1,9 @@
+string_one = input("Please type in string 1: ")
+string_two = input("Please type in string 2: ")
+
+if len(string_one) > len(string_two):
+    print(f"{string_one} is longer")
+elif len(string_one) < len(string_two):
+    print(f"{string_two} is longer")
+else:
+    print("The strings are equally long")
